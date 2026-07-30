62937226617ecd33eaaa124dbcf124fa4bf9322e
darkfirebtw <121009303+C0ms@users.noreply.github.com>
2026-07-29 17:59:19 -0700

    Updated ReplicatedStorage/Packages/DataService/Signal/wally.lua

---
+++ ReplicatedStorage/Packages/DataService/Signal/wally.lua
@@ -0,0 +1,13 @@
+-- @ScriptType: ModuleScript
+return {
+	package = {
+		authors = {"Stephen Leitnick"},
+		description = "Signal class",
+		exclude = {"node_modules", "package.json", "**/*.ts"},
+		license = "MIT",
+		name = "sleitnick/signal",
+		realm = "shared",
+		registry = "https://github.com/UpliftGames/wally-index",
+		version = "2.0.3",
+	},
+}

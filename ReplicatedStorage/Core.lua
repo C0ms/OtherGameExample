f4dfa79703c76109d67cd7df10a1e71fe7569dfc
darkfirebtw <121009303+C0ms@users.noreply.github.com>
2026-07-29 17:59:09 -0700

    Updated ReplicatedStorage/Core.lua

---
+++ ReplicatedStorage/Core.lua
@@ -0,0 +1,16 @@
+-- @ScriptType: ModuleScript
+
+--[[
+	Framework developed by
+    .___             __      _____.__              ___.    __           
+  __| _/____ _______|  | ___/ ____\__|______   ____\_ |___/  |___  _  __
+ / __ |\__  \\_  __ \  |/ /\   __\|  \_  __ \_/ __ \| __ \   __\ \/ \/ /
+/ /_/ | / __ \|  | \/    <  |  |  |  ||  | \/\  ___/| \_\ \  |  \     / 
+\____ |(____  /__|  |__|_ \ |__|  |__||__|    \___  >___  /__|   \/\_/       
+]]
+
+local server = require(script.CoreServer)
+
+export type server = server.CoreServer
+
+return server

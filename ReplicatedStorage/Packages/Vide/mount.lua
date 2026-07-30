2603432c587c4549bdf155a181adb14f262ddbbf
darkfirebtw <121009303+C0ms@users.noreply.github.com>
2026-07-29 17:59:00 -0700

    Updated ReplicatedStorage/Packages/Vide/mount.lua

---
+++ ReplicatedStorage/Packages/Vide/mount.lua
@@ -0,0 +1,12 @@
+-- @ScriptType: ModuleScript
+local root = require "./root"
+local apply = require "./apply"
+
+local function mount<T>(component: () -> T, target: Instance?): () -> ()
+    return root(function()
+        local result = component()
+        if target then apply(target, { result }) end
+    end)
+end
+
+return mount :: (<T>(component: () -> T, target: Instance) -> () -> ()) & ((component: () -> ()) -> () -> ())

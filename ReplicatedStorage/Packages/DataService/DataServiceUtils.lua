2232784e0458ff860cb0bc36c2ee6702541fb5c9
darkfirebtw <121009303+C0ms@users.noreply.github.com>
2026-07-29 17:59:23 -0700

    Updated ReplicatedStorage/Packages/DataService/DataServiceUtils.lua

---
+++ ReplicatedStorage/Packages/DataService/DataServiceUtils.lua
@@ -0,0 +1,13 @@
+-- @ScriptType: ModuleScript
+local DataServiceUtils = {}
+
+DataServiceUtils.enums = {
+	actions = {
+		init = 0,
+		set = 1,
+		arrayInsert = 2,
+		arrayRemove = 3,
+	},
+}
+
+return DataServiceUtils

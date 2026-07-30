58d1e8690f436be81ef01feeb98724db2dc03b15
darkfirebtw <121009303+C0ms@users.noreply.github.com>
2026-07-29 17:58:00 -0700

    Updated ReplicatedStorage/Packages/DataService/Networker.lua

---
+++ ReplicatedStorage/Packages/DataService/Networker.lua
@@ -0,0 +1,11 @@
+-- @ScriptType: ModuleScript
+local server = require(script.NetworkerServer)
+local client = require(script.NetworkerClient)
+
+export type Server = server.NetworkerServer
+export type Client = client.NetworkerClient
+
+return {
+	server = server,
+	client = client,
+}
